8e8b1b5cbeca546b86adcd7bbe4aeb58a85060e4
FazeElian <elian.ibarra@soy.sena.edu.co>
2026-04-05 12:18:08 -0500

    init: react query client & services query files structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,28 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Global styles
 import './assets/css/global.css'
+
+// Router comp
 import Router from './Router'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    }
+  }
+});
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <Router />
     </BrowserRouter>
-  </StrictMode>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 )
