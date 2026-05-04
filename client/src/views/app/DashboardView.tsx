d87f337902b61120928e271ea6a66bc99f30b577
FazeElian <elian.ibarra@soy.sena.edu.co>
2026-05-03 22:51:18 -0500

    feat: show only first name in dashboard welcome message

--- client/src/views/app/DashboardView.tsx
+++ client/src/views/app/DashboardView.tsx
@@ -4,11 +4,19 @@ import { QuickStats } from "@/components/app/molecules/QuickStats"
 import { SnippetsGallery } from "@/components/app/molecules/SnippetsGallery"
 import { SnippetsHeader } from "@/components/app/molecules/SnippetsHeader"
 
+// Get user from gloal state (context)
+import { useUser } from "@/services/users/context"
+
 const DashboardView = () => {
+    const { user } = useUser();
+
+    if (!user) return null;
+    const firstName = user.fullName.split(" ")[0];
+
     return (
         <main className="app-content">
             <PageTitle
-                title="Welcome Back, John"
+                title={`Welcome Back, ${firstName} 👋`}
                 subtitle="My Snippets"
                 button={true}
             />
