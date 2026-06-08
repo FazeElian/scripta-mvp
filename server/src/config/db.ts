0aa78beebaf59cca77d6907104e5a152e2a6da65
FazeElian <elian.ibarra@soy.sena.edu.co>
2026-06-08 11:41:49 -0500

    chore: container & db settings for local db & finish MVP

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -2,14 +2,17 @@ import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
 export const db = new Sequelize (process.env.DATABASE_URL, {
     models: [__dirname + "/../models/**/*"],
     logging: false,
     dialect: "postgres",
-    dialectOptions: {
+  dialectOptions: isProduction
+    ? {
         ssl: {
-            require: false,
-            rejectUnauthorized: false,
-        }
-    }
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
 })
